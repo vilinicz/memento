7c01907e71dda0075544ab63d5117a380bcb4c00
slavikse <gslavikse@gmail.com>
2019-01-21 16:30:39 +0300

    Выведены счётчики fps и времени выполнения распознователя.
    Рефакторинг.

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,20 @@
     <video
       ref="video"
       autoplay
-      muted
       hidden
-      @play="detect"
+      @play="detectFaces"
     />
 
     <div
+      v-if="avgTime !== -1"
+      class="stats"
+    >
+      <p>{{ avgTime }} ms | цель: {{ (1000 / maxFps).toFixed() }}</p>
+      <p>{{ fps }} fps | цель: {{ maxFps }}</p>
+    </div>
+
+    <div
+      v-if="avgTime !== -1"
       class="screenfull"
       @mousedown="enableScreenFull"
       @touchstart="enableScreenFull"
@@ -22,62 +30,85 @@
 import * as faceapi from 'face-api.js';
 import screenfull from 'screenfull';
 
+const maxFps = 15;
+let forwardTimes = [];
+
 export default {
   name: 'App',
 
   data() {
     return {
-      ssdMobilenetv1Options: {
-        minConfidence: 0.5, // 0.5
-        maxResults: 5, // 5
-      },
-      tinyFaceDetectorOptions: {
-        inputSize: 512, // 512
-        scoreThreshold: 0.5, // 0.5
+      maxFps,
+
+      constraints: {
+        audio: false,
+        video: { frameRate: { max: maxFps } },
       },
 
       // При экспериментах, не забыть поменять загрузку!
       options: new faceapi.SsdMobilenetv1Options(this.ssdMobilenetv1Options),
       // options: new faceapi.TinyFaceDetectorOptions(this.tinyFaceDetectorOptions),
 
-      constraints: {
-        audio: false,
-        video: {
-          frameRate: {
-            ideal: 10,
-            max: 15,
-          },
-        },
+      ssdMobilenetv1Options: {
+        minConfidence: 0.5,
+        maxResults: 5,
+      },
+      tinyFaceDetectorOptions: {
+        inputSize: 512,
+        scoreThreshold: 0.5,
       },
 
-      timeout: 1000 / 15,
+      avgTime: -1,
+      fps: -1,
+
+      timeout: 1000 / maxFps,
     };
   },
 
   async mounted() {
-    const modelUri = '/models';
-
-    // Точнее, но медленнее.
-    await faceapi.loadSsdMobilenetv1Model(modelUri);
-    // await faceapi.loadTinyFaceDetectorModel(modelUri);
-
-    await faceapi.loadFaceLandmarkModel(modelUri);
-    await faceapi.loadFaceRecognitionModel(modelUri);
-
-    this.$refs.video.srcObject = await navigator.mediaDevices.getUserMedia(this.constraints);
+    await this.loadingModels();
+    await this.getUserMedia();
   },
 
   methods: {
-    async detect() {
+    async loadingModels() {
+      const modelUri = '/models';
+
+      // Точнее, но медленнее.
+      await faceapi.loadSsdMobilenetv1Model(modelUri);
+      // await faceapi.loadTinyFaceDetectorModel(modelUri);
+
+      await faceapi.loadFaceLandmarkModel(modelUri);
+      await faceapi.loadFaceRecognitionModel(modelUri);
+    },
+
+    async getUserMedia() {
+      this.$refs.video.srcObject = await navigator.mediaDevices.getUserMedia(this.constraints);
+    },
+
+    async detectFaces() {
+      const prevTime = performance.now();
+
       const results = await faceapi.detectAllFaces(this.$refs.video, this.options)
         .withFaceLandmarks()
         .withFaceDescriptors();
+
+      this.updateStats(prevTime);
 
       if (results.length > 0) {
         this.collectDescriptors(results);
       }
 
-      setTimeout(this.detect, this.timeout);
+      setTimeout(this.detectFaces, this.timeout);
+    },
+
+    updateStats(prevTime) {
+      const time = performance.now() - prevTime;
+      forwardTimes = [time, ...forwardTimes].slice(0, 30);
+      const avgTime = forwardTimes.reduce((total, t) => total + t) / forwardTimes.length;
+
+      this.avgTime = Math.round(avgTime);
+      this.fps = Math.round(1000 / avgTime);
     },
 
     // todo хитрый сохранятор.
@@ -104,6 +135,17 @@ export default {
 .App {
   height: 100vh;
   background-color: #333;
+}
+
+.stats {
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+  padding: 0.1rem 0.5rem 0.3rem;
+  border-radius: 1rem;
+  font-size: 1rem;
+  color: white;
+  background-color: darkslategray;
 }
 
 .screenfull {
